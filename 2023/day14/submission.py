2ac13d302e2148a5bf1c825889f60ad6d11dae8c
Your Name <you@example.com>
2023-02-06 16:41:23 +0530

    day 14 submission

--- 2023/day14/submission.py
+++ 2023/day14/submission.py
@@ -1,10 +1,12 @@
 fav_tools = {1:"Linux", 2:"Git", 3:"Docker", 4:"Kubernetes", 5:"Terraform", 6:"Ansible", 7:"Chef"}
+print("The values of the Fav Tools are: ")
 for key in fav_tools:
-  print(fav_tools[key])
+    
+    print(fav_tools[key])
 
 cloud_providers = ['Amazon Web Services (AWS)', 'Microsoft Azure', 'Google Cloud Platform (GCP)', 'IBM Cloud', 'Alibaba Cloud', 'Oracle Cloud']
 
 cloud_providers.append("DigitalOcean")
 cloud_providers.sort()
-
+print("\n The sorted list of cloud providers are: ")
 print(cloud_providers)
